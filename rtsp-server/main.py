be94a6813cceeeffda11a0f1ab643d2130581993
Dave Russell <daverussell1309@gmail.com>
2023-10-12 22:23:46 +0700

    refactor(main.py): create server logs and refactoring server initialization

--- rtsp-server/main.py
+++ rtsp-server/main.py
@@ -10,71 +10,77 @@ from gi.repository import Gst, GstRtspServer, GLib
 class Stream:
     def __init__(
         self,
-        server,
+        name,
         pipeline,
         endpoint
     ):
         self.server = server
+        self.name = name
         self.pipeline = pipeline
         self.endpoint = endpoint
 
-    def video_stream(self):
+    def register_stream(self, server):
         factory = GstRtspServer.RTSPMediaFactory.new()
         factory.set_launch(self.pipeline)
         factory.set_shared(True)
-        self.server.get_mount_points().add_factory(self.endpoint, factory)
+        server.get_mount_points().add_factory(self.endpoint, factory)
 
-def main():
+class RTSPServer:
+    def __init__(self, address, port):
+        self.server = GstRtspServer.RTSPServer.new()
+        self.server.set_address(address)
+        self.server.set_service(port)
+        self.server.attach(None)
+        self.streams = []
+
+    def add_new_stream(self, name, pipeline, path):
+        new_stream = Stream(name, pipeline, path)
+        new_stream.register_stream(self.server)
+        self.streams.append(new_stream)
+
+    def stream_list(self):
+        print("==============================================")
+        index = 1
+        for stream in self.streams:
+            url = f"rtsp://{self.server.get_address()}:{self.server.get_service()}{stream.endpoint}"
+            print(f"{index}. {stream.name} | {url}")
+            index += 1
+
+if __name__ == "__main__":
     Gst.init(None)
 
     server_address = socket.gethostbyname(socket.gethostname())
     server_port = "8554"
 
-    server = GstRtspServer.RTSPServer.new()
-    server.set_address(server_address)
-    server.set_service(server_port)
-
-    # RTSP path for vid1
-    pipeline_vid1 = (
-        "filesrc location=./videos/vid1.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96"
+    server = RTSPServer(server_address, server_port)
+    server.add_new_stream(
+        "Video Pantai",
+        "filesrc location=./videos/vid1.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96",
+        "/vid1"
     )
-    stream_1 = Stream(server, pipeline_vid1, '/vid1')
-    stream_1.video_stream()
-
-
-    # RTSP path for vid2
-    pipeline_vid2 = (
-        "filesrc location=./videos/vid2.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96"
+    server.add_new_stream(
+        "Video Air",
+        "filesrc location=./videos/vid2.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96",
+        '/vid2'
     )
-    stream_2 = Stream(server, pipeline_vid2, '/vid2')
-    stream_2.video_stream()
-
-
-    # RTSP path for vid3
-    pipeline_vid3 = (
-        "filesrc location=./videos/vid3.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96"
+    server.add_new_stream(
+        "Video Senja",
+        "filesrc location=./videos/vid3.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96",
+        "/vid3"
     )
-    stream_3 = Stream(server, pipeline_vid3, '/vid3')
-    stream_3.video_stream()
-
-
-    # RTSP path for vid4
-    pipeline_vid4 = (
-        "filesrc location=./videos/vid4.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96"
+    server.add_new_stream(
+        "Video Tempat Sampah",
+        "filesrc location=./videos/vid4.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96",
+        "/vid4"
     )
-    stream_4 = Stream(server, pipeline_vid4, '/vid4')
-    stream_4.video_stream()
-
-
-    # RTSP path for webcam
-    pipeline_webcam = (
-        "ksvideosrc device-index=0 ! videoconvert ! x264enc tune=zerolatency key-int-max=15 bitrate=1000 ! h264parse ! rtph264pay name=pay0 pt=96 mtu=1200 ! rtspclientsink location=rtsp://127.0.0.1:8554/live sync=false"
+    server.add_new_stream(
+        "Webcam",
+        "ksvideosrc device-index=0 ! videoconvert ! x264enc tune=zerolatency key-int-max=15 bitrate=1000 ! h264parse ! rtph264pay name=pay0 pt=96 mtu=1200",
+        "/cam"
     )
-    stream_4 = Stream(server, pipeline_webcam, '/live')
-    stream_4.video_stream()
 
     print(f"RTSP Server started on {server_address}:{server_port}")
-    server.attach(None)
+    server.stream_list()
 
     main_loop = GLib.MainLoop()
     try:
@@ -82,5 +88,6 @@ def main():
     except KeyboardInterrupt:
         main_loop.quit()
 
-if __name__ == "__main__":
-    main()
+
+
+
